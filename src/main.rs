8b2836ed0c4971edea4ea212a932d96ef38d5cda
Alexey Vasiliev <a.v.vasiliev@gmail.com>
2024-12-16 08:48:31 +0300

    a tensor is already on cpu by default

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@ use tch::{Device, Cuda, Tensor};
 
 fn main() {
     let gpu = Device::cuda_if_available();
-    let cpu = Device::Cpu;
     println!("Текущее устройство: {:?}", gpu);
     println!("Текущее CudaNN: {:?}", Cuda::cudnn_is_available());
     let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
     let t = t * 2;
     let tc = t.to_device(gpu);
-    let cc = t.to_device(cpu);
     tc.print();
-    cc.print();
+    t.print();
 }
   
