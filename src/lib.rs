50d61acdc1a8538bab86fbc4369b5e9339ab99bc
Alexey Vasiliev <a.v.vasiliev@gmail.com>
2024-12-18 14:35:23 +0300

    resnet

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
-extern crate tch;
-use tch::{Device, Cuda, Tensor};
-pub fn cpu_gpu() {
-    let gpu = Device::cuda_if_available();
-    println!("Текущее устройство: {:?}", gpu);
-    println!("Текущее CudaNN: {:?}", Cuda::cudnn_is_available());
-    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
-    let t = t * 2;
-    let tc = t.to_device(gpu);
-    tc.print();
-    t.print();
+mod nn;
+pub fn run_cpu_gpu() {
+    nn::cpu_gpu();
 }
